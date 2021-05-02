61aac2a28732bd5ac63d9ceda6c9635236752624
Stan <iamjacke@gmail.com>
2021-05-02 21:59:51 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# Templates for alder 
+
+to view all necesary infos
+
+## Scala project
+
+```
+alder --exclude=".git|bower_components|node_modules|target"
+```
+
+Example(hidden): 
+```
+
+```
---
+++ detect.rs
@@ -0,0 +1,25 @@
+use std::process::Command;
+
+
+fn main() {
+
+let mut list_dir = Command::new("ls");
+
+// Execute `ls` in the current directory of the program.
+list_dir.status().expect("process failed to execute");
+
+println!();
+
+// Change `ls` to execute in the root directory.
+list_dir.current_dir("/");
+
+// And then execute `ls` again but in the root directory.
+list_dir.status().expect("process failed to execute");
+///////////////////////////////////////////////////////////////
+// Execute this
+
+// For scala project
+// # alder --exclude=".git|bower_components|node_modules|target"
+//////
+
+}
